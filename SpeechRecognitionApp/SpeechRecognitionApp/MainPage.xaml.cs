85f8cf230b25de88b8ddd857eb12386405b95e26
Mahima Bhat <bhatmahima01@gmail.com>
2020-06-08 13:13:00 +0530

    Revert "Added 3 pages"
    This reverts commit 158c40ff5968239b2f8bd61dd465312058dd0a69.

--- SpeechRecognitionApp/SpeechRecognitionApp/MainPage.xaml.cs
+++ SpeechRecognitionApp/SpeechRecognitionApp/MainPage.xaml.cs
@@ -16,19 +16,6 @@ namespace SpeechRecognitionApp
         public MainPage()
         {
             InitializeComponent();
-            MyButton.ImageSource = ImageSource.FromResource("SpeechRecognitionApp.Images.mic.png");
-        }
-
-        private void MyButton_Pressed(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MyButton_Released(object sender, EventArgs e)
-        {
-
         }
     }
-
-    
 }
