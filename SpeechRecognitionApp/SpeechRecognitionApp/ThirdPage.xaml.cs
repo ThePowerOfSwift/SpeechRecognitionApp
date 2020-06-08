f2b6a2ad376bafc71e8627788bd93e7a40a1661e
Mahima Bhat <bhatmahima01@gmail.com>
2020-06-08 13:47:59 +0530

    Added UI for 3 pages

---
+++ SpeechRecognitionApp/SpeechRecognitionApp/ThirdPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SpeechRecognitionApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ThirdPage : ContentPage
+    {
+        public ThirdPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ThirdButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
