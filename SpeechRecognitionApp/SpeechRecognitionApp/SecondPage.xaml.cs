0048c97a26a8cde7ac1bcd92648cd1bda68b1017
Mahima Bhat <bhatmahima01@gmail.com>
2020-06-08 16:23:41 +0530

    Added code for second page and third page ..

--- SpeechRecognitionApp/SpeechRecognitionApp/SecondPage.xaml.cs
+++ SpeechRecognitionApp/SpeechRecognitionApp/SecondPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
+using VoiceToCommand;
 using Xamarin.Forms.Xaml;
 
 namespace SpeechRecognitionApp
@@ -12,14 +8,87 @@ namespace SpeechRecognitionApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SecondPage : ContentPage
     {
+        private IVoiceToCommandService speechToTextService;
+        
         public SecondPage()
         {
             InitializeComponent();
+            try
+            {
+                speechToTextService = DependencyService.Get<IVoiceToCommandService>();
+                RegisterVoiceCommands();
+               
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+        }
+
+        private void NavigateToFirstPage()
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void NavigateToThirdPage()
+        {
+            Navigation.PushAsync(new ThirdPage());
+        }
+
+        private void RegisterVoiceCommands()
+        {
+            speechToTextService.RegisterCommand("Hello", new VoiceCommand(() => { SpeechToTextFinalResultRecieved("Command is 1:Hello"); }));
+            speechToTextService.RegisterCommand("Back", new VoiceCommand(NavigateToFirstPage));
+            speechToTextService.RegisterCommand("Next", new VoiceCommand(NavigateToThirdPage));
+        }
+
+        
+
+        private void SpeechToTextFinalResultRecieved(string args)
+        {
+            recon.Text = args;
         }
 
         private void SecondButton_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                speechToTextService.StartListening();
+            }
 
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                MyButton.IsEnabled = false;
+            }
+
+        }
+
+        private void OnSliderValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public class VoiceCommand : IVoiceCommand
+        {
+            private Action _action;
+            public VoiceCommand(Action action)
+            {
+                _action = action;
+            }
+            public bool CanExecute()
+            {
+                return true;
+            }
+
+            public void Execute()
+            {
+                _action.Invoke();
+            }
         }
     }
 }
--- SpeechRecognitionApp/SpeechRecognitionApp/ThirdPage.xaml.cs
+++ SpeechRecognitionApp/SpeechRecognitionApp/ThirdPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
+using VoiceToCommand;
 using Xamarin.Forms.Xaml;
 
 namespace SpeechRecognitionApp
@@ -12,14 +8,83 @@ namespace SpeechRecognitionApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ThirdPage : ContentPage
     {
+
+        private IVoiceToCommandService speechToTextService;
         public ThirdPage()
         {
             InitializeComponent();
+            try
+            {
+                speechToTextService = DependencyService.Get<IVoiceToCommandService>();
+                RegisterVoiceCommands();
+
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
         }
+
+        private void NavigateToFirstPage()
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void NavigateToSecondPage()
+        {
+            Navigation.PushAsync(new SecondPage());
+        }
+
+        private void RegisterVoiceCommands()
+        {
+            speechToTextService.RegisterCommand("Hello", new VoiceCommand(() => { SpeechToTextFinalResultRecieved("Command is 1:Hello"); }));
+            speechToTextService.RegisterCommand("Home", new VoiceCommand(NavigateToFirstPage));
+            speechToTextService.RegisterCommand("Back", new VoiceCommand(NavigateToSecondPage));
+        }
+
+
+
+        private void SpeechToTextFinalResultRecieved(string args)
+        {
+            recon.Text = args;
+        }
+
 
         private void ThirdButton_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                speechToTextService.StartListening();
+            }
 
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                MyButton.IsEnabled = false;
+            }
+
+        }
+
+        public class VoiceCommand : IVoiceCommand
+        {
+            private Action _action;
+            public VoiceCommand(Action action)
+            {
+                _action = action;
+            }
+            public bool CanExecute()
+            {
+                return true;
+            }
+
+            public void Execute()
+            {
+                _action.Invoke();
+            }
         }
     }
 }
