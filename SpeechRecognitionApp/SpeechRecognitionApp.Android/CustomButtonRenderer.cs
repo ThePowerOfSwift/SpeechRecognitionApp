fb9a84d76f35cc3557c109e2ea7e84e0fb5511b5
Mahima Bhat <bhatmahima01@gmail.com>
2020-06-08 14:29:16 +0530

    Added Code for mainpage

---
+++ SpeechRecognitionApp/SpeechRecognitionApp.Android/CustomButtonRenderer.cs
@@ -0,0 +1,32 @@
+﻿
+using Android.Views;
+using SpeechRecognitionApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Widget;
+
+namespace SpeechRecognitionApp.Droid
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            var customButton = e.NewElement as CustomButton;
+
+            var thisButton = Control as Android.Widget.Button;
+            thisButton.Touch += (object sender, TouchEventArgs args) =>
+            {
+                if (args.Event.Action == MotionEventActions.Down)
+                {
+                    customButton.OnPressed();
+                }
+                else if (args.Event.Action == MotionEventActions.Up)
+                {
+                    customButton.OnReleased();
+                }
+            };
+        }
+    }
+}
--- SpeechRecognitionApp/SpeechRecognitionApp/MainPage.xaml.cs
+++ SpeechRecognitionApp/SpeechRecognitionApp/MainPage.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using VoiceToCommand;
+using Xamarin.Essentials;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using PermissionStatus = Plugin.Permissions.Abstractions.PermissionStatus;
 
 namespace SpeechRecognitionApp
 {
@@ -13,21 +14,117 @@ namespace SpeechRecognitionApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private IVoiceToCommandService speechToTextService;
+        private bool isPermissionGranted;
         public MainPage()
         {
             InitializeComponent();
-            MyButton.ImageSource = ImageSource.FromResource("SpeechRecognitionApp.Images.mic.png");
+            
+            try
+            {
+                speechToTextService = DependencyService.Get<IVoiceToCommandService>();
+                RegisterVoiceCommands();
+                MyButton.ImageSource = ImageSource.FromResource("SpeechRecognitionApp.Images.mic.png");
+                CheckPermissionStatus();
+                SpeakInitialInstruction();
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
 
         }
 
+        private void RegisterVoiceCommands()
+        {
+            speechToTextService.RegisterCommand("Hello", new VoiceCommand(() => { SpeechToTextFinalResultRecieved("Command is 1:Hello"); }));
+            speechToTextService.RegisterCommand("Next", new VoiceCommand(() => { SpeechToTextFinalResultRecieved("Command is 2:Go Next"); }));
+            speechToTextService.RegisterCommand("Back", new VoiceCommand(() => { SpeechToTextFinalResultRecieved("Command is 3:Go Back"); }));
+        }
+
+        private async void CheckPermissionStatus()
+        {
+            var permissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Microphone);
+            if (PermissionStatus.Granted == permissionStatus)
+            {
+                isPermissionGranted = true;
+            }
+            else
+            {
+                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Microphone))
+                {
+                    await DisplayAlert("Need Mic", "Need Microphone Access", "OK");
+                }
+
+                var status = await CrossPermissions.Current.RequestPermissionAsync<MicrophonePermission>();
+                if (status == PermissionStatus.Granted)
+                {
+                    isPermissionGranted = true;
+                }
+
+            }
+        }
+
+        private void SpeakInitialInstruction()
+        {
+            TextToSpeech.SpeakAsync("To Speak Press and Hold the microphone Image. Release when done!");
+        }
+
+        private void SpeechToTextFinalResultRecieved(string args)
+        {
+            recon.Text = args;
+        }
+
+
         private void MyButton_Pressed(object sender, EventArgs e)
         {
+            try
+            {
+                if (isPermissionGranted)
+                {
+                    MyButton.ImageSource = ImageSource.FromResource("SpeechRecognitionApp.Images.mic.png");
+                    speechToTextService.StartListening();
+                }
+                else
+                {
+                    CheckPermissionStatus();
+                }
+            }
+            catch (Exception ex)
+            {
+                recon.Text = ex.Message;
+            }
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                MyButton.IsEnabled = false;
+            }
 
         }
 
         private void MyButton_Released(object sender, EventArgs e)
         {
+            MyButton.ImageSource = ImageSource.FromResource("SpeechRecognitionApp.Images.mic.png");
+            speechToTextService.StopListening();
 
+        }
+
+        public class VoiceCommand : IVoiceCommand
+        {
+            private Action _action;
+            public VoiceCommand(Action action)
+            {
+                _action = action;
+            }
+            public bool CanExecute()
+            {
+                return true;
+            }
+
+            public void Execute()
+            {
+                _action.Invoke();
+            }
         }
     }
 }
