905752801c60b37d395927f1cfeece4e9a6db579
Mahima Bhat <bhatmahima01@gmail.com>
2020-06-09 10:12:45 +0530

    Added Splash Activity

--- SpeechRecognitionApp/SpeechRecognitionApp.Android/MainActivity.cs
+++ SpeechRecognitionApp/SpeechRecognitionApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace SpeechRecognitionApp.Droid
 {
-    [Activity(Label = "SpeechRecognitionApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SpeechRecognitionApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
